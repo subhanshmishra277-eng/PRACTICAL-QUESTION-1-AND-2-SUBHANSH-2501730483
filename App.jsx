253e39c60b4f96eb09122ab9429edb37a8c3c2da
subhanshmishra277-eng <subhanshmishra277@gmail.com>
2026-05-05 13:18:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import DataFetcher from './DataFetcher';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First API Fetch</h1>
+      <DataFetcher/>
+    </div>
+  );
+}
+
+export default App; al
